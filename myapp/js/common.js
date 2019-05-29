12b3d3181bbc3486f2679fd41e456b0d3fa34b93
Helen <liaforweb@gmail.com>
2019-05-29 08:57:19 +0300

    mob 4-8

--- myapp/js/common.js
+++ myapp/js/common.js
@@ -12,10 +12,22 @@ if(jQuery('.lic__slider').length) {
         {
           breakpoint: 1000,
           settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },   {
+          breakpoint: 670,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        } ,   {
+          breakpoint: 500,
+          settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
           }
-        }  
+        }
 
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
